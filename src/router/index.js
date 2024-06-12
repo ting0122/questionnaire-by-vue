a505cc478de21c9714d3d1c51e977fc59eec43ff
chen <ting0919764920@gmail.com>
2024-06-12 16:38:27 +0800

    try swagger： swagger function
    - change router but canceled

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ListActivity from '@/views/ActivityContainer.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
